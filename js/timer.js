cbbe4b36b6dbaf6227e861b1aa24864d740d550b
ssonggyu <ssonggyu@gmail.com>
2018-11-29 10:40:52 +0900

    update

--- js/timer.js
+++ js/timer.js
@@ -22,28 +22,28 @@ function numChange(){
 	var date = new Date();
 	var cN = document.getElementsByClassName('num')[0];
 
-	console.log(cN.id);
+	console.log(cN.className);
 
 	if(date.getSeconds() == "1"){
-		cN.id = "num1";
+		cN.className = "num1";
 	}else if(date.getSeconds() == "2"){
-		cN.id = "num2";
+		cN.className = "num2";
 	}else if(date.getSeconds() == "3"){
-		cN.id = "num3";
+		cN.className = "num3";
 	}else if(date.getSeconds() == "4"){
-		cN.id = "num4";
+		cN.className = "num4";
 	}else if(date.getSeconds() == "5"){
-		cN.id = "num5";
+		cN.className = "num5";
 	}else if(date.getSeconds() == "6"){
-		cN.id = "num6";
+		cN.className = "num6";
 	}else if(date.getSeconds() == "7"){
-		cN.id = "num7";
+		cN.className = "num7";
 	}else if(date.getSeconds() == "8"){
-		cN.id = "num8";
+		cN.className = "num8";
 	}else if(date.getSeconds() == "9"){
-		cN.id = "num9";
+		cN.className = "num9";
 	}else{
-		cN.id = "num0";
+		cN.className = "num0";
 	}
 
 }
