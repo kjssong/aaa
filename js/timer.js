8fd887270dbb775ef12d85cfd7c6b12057c32472
ssonggyu <ssonggyu@gmail.com>
2018-12-03 10:25:51 +0900

    update

--- js/timer.js
+++ js/timer.js
@@ -1,9 +1,5 @@
 window.onload = function(){
-	startInterval();	
-	//printCurrentDate();
-
     numChange();
-    timer();
 }
 function startInterval() {
 	setInterval(numChange, 1000);
@@ -18,65 +14,50 @@ function printCurrentDate() {
 	document.getElementById("s").innerHTML = date.getSeconds();
 }
 function numChange(){
-    var date = new Date();
-    var cNa = document.getElementById('numA');
-    var cNb = document.getElementById('numB');
-    var dateArray = date.getSeconds().toString().split('');
-    
-    if(dateArray <= 9){
-        dateArray = "0" + dateArray;
-    }
+        var date = new Date();
+        var tSa = document.getElementById('numSA');
+        var tSb = document.getElementById('numSB');
+        var dateArray = date.getSeconds().toString().split('');
 
-    /*초 - 첫번째 자리*/
-    if(dateArray[0] == "1"){
-        cNa.className = "num1";
-	}else if(dateArray[0] == "2"){
-        cNa.className = "num2";
-	}else if(dateArray[0] == "3"){
-        cNa.className = "num3";
-	}else if(dateArray[0] == "4"){
-        cNa.className = "num4";
-	}else if(dateArray[0] == "5"){
-        cNa.className = "num5";
-	}else{
-		cNa.className = "num0";
-    }
-    
-    /*초 - 두번째 자리*/
-	if(dateArray[1] == "1"){
-        cNb.className = "num1";
-	}else if(dateArray[1] == "2"){
-        cNb.className = "num2";
-	}else if(dateArray[1] == "3"){
-        cNb.className = "num3";
-	}else if(dateArray[1] == "4"){
-        cNb.className = "num4";
-	}else if(dateArray[1] == "5"){
-        cNb.className = "num5";
-	}else if(dateArray[1] == "6"){
-        cNb.className = "num6";
-	}else if(dateArray[1] == "7"){
-        cNb.className = "num7";
-	}else if(dateArray[1] == "8"){
-        cNb.className = "num8";
-	}else if(dateArray[1] == "9"){
-        cNb.className = "num9";
-	}else{
-		cNb.className = "num0";
-	}
-}
-
-
-function timer(){
-    var time = 10;
-    var timeSec = function(){
-        time--;
-        
-        if(time == 5){
-            console.log("5");
+        if(dateArray <= 9){
+                dateArray = "0" + dateArray;
         }
 
-        document.getElementById("timer").innerHTML = time;
-    }
-    setInterval(timeSec, 1000);
+        /*초 - 첫번째 자리*/
+        if(dateArray[0] == "1"){
+                tSa.className = "num1";
+        }else if(dateArray[0] == "2"){
+                tSa.className = "num2";
+        }else if(dateArray[0] == "3"){
+                tSa.className = "num3";
+        }else if(dateArray[0] == "4"){
+                tSa.className = "num4";
+        }else if(dateArray[0] == "5"){
+                tSa.className = "num5";
+        }else{
+                tSa.className = "num0";
+        }
+
+        /*초 - 두번째 자리*/
+        if(dateArray[1] == "1"){
+                tSb.className = "num1";
+        }else if(dateArray[1] == "2"){
+                tSb.className = "num2";
+        }else if(dateArray[1] == "3"){
+                tSb.className = "num3";
+        }else if(dateArray[1] == "4"){
+                tSb.className = "num4";
+        }else if(dateArray[1] == "5"){
+                tSb.className = "num5";
+        }else if(dateArray[1] == "6"){
+                tSb.className = "num6";
+        }else if(dateArray[1] == "7"){
+                tSb.className = "num7";
+        }else if(dateArray[1] == "8"){
+                tSb.className = "num8";
+        }else if(dateArray[1] == "9"){
+                tSb.className = "num9";
+        }else{
+                tSb.className = "num0";
+        }
 }
